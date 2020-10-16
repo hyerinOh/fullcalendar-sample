6c63f04d5158a073f7170f367b0236ac43ed8d96
hyerinOh <hyerinOh1108@gmail.com>
2020-10-16 18:42:18 +0900

    apply event utils

--- fullcalendar-sample/src/components/Fullcalendar.tsx
+++ fullcalendar-sample/src/components/Fullcalendar.tsx
@@ -4,17 +4,57 @@ import Fullcalendar, {
   DateSelectArg,
   EventClickArg,
   EventContentArg,
-  formatDate,
 } from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { INITIAL_EVENTS, createEventId } from '../utils/event-utils';
 import Sidebar from './Sidebar';
 import './Fullcalendar.scss';
 
 function FullcalendarComponent() {
   const [weekendVisible, setWeekendVisible] = useState(true);
-  const [currentEvents, setCurrentEvents] = useState([]);
+  const [currentEvents, setCurrentEvents] = useState<EventApi[]>([]);
+
+  const handleDateSelect = (selectInfo: DateSelectArg) => {
+    let title = prompt('Please enter a new title for your event');
+
+    const calendarApi = selectInfo.view.calendar;
+    calendarApi.unselect();
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };
+
+  const renderEventContent = (eventContent: EventContentArg) => {
+    return (
+      <>
+        <b>{eventContent.timeText}</b>
+        <i>{eventContent.event.title}</i>
+      </>
+    );
+  };
+
+  const handleEventClick = (clickInfo: EventClickArg) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  };
+
+  const handleEvents = (events: EventApi[]) => {
+    setCurrentEvents(events);
+  };
 
   return (
     <div className="fullcalendar-wrapper">
@@ -37,11 +77,11 @@ function FullcalendarComponent() {
           selectMirror={true}
           dayMaxEvents={true}
           weekends={weekendVisible}
-          // initialEvents={}
-          // select={}
-          // eventContent={}
-          // eventClick={}
-          // eventsSet={}
+          initialEvents={INITIAL_EVENTS}
+          select={handleDateSelect}
+          eventContent={renderEventContent}
+          eventClick={handleEventClick}
+          eventsSet={handleEvents}
         />
       </div>
     </div>
--- fullcalendar-sample/src/components/Sidebar.tsx
+++ fullcalendar-sample/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { EventApi, formatDate } from '@fullcalendar/react';
 import './Sidebar.scss';
 
